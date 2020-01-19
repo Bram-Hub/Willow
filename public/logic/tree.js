f8bf694c02adc5f492f42ed3272967e4050e4c12
Connor Roizman <connorjayr@gmail.com>
2020-01-19 12:07:49 -0500

    Add helper function to determine if a branch can be closed

--- public/logic/tree.js
+++ public/logic/tree.js
@@ -78,3 +78,35 @@ class TruthTree {
     return completed;
   }
 }
+
+/**
+ * Determines if a branch is able to be closed, which is when the branch contains
+ * both a literal and its negation.
+ * 
+ * @param {Statement[]} branch the branch
+ * @returns {boolean} if the branch can be closed
+ */
+function canCloseBranch(branch) {
+  // convert the branch to a set of strings for constant-time lookup
+  // TODO: define toString for Statement
+  branch = Set(branch.map(statement => statement.toString()));
+
+  for (const statement of branch) {
+    if (statement.isLiteral()) {
+      // if the statement is a literal, check if the branch contains its negation
+      if (statement instanceof NotStatement &&
+          branch.has(statement.operand.toString())) {
+        // if the statement is a NotStatement, check if the branch contains its
+        // operand
+        return true;
+      } else if (branch.has(new NotStatement(statement).toString())) {
+        // if the statement is an AtomicStatement, check if the branch contains
+        // its negation
+        return true;
+      }
+    }
+  }
+  // if none of the statements had a negation contained in the branch, then the
+  // branch cannot be closed
+  return false;
+}
