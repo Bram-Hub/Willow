52bd92c212603fafb307c570dfe0d1e46729156a
Connor Roizman <connorjayr@gmail.com>
2020-03-22 07:49:56 -0400

    Make expand arrow a button element for improved accessibility

--- public/index.js
+++ public/index.js
@@ -38,7 +38,7 @@ const vm = new Vue({
 <ul class="node-list">
   <li v-for="(statement, idx) in node.statements">
     <input v-if="idx == 0 || expanded" v-model="statement.str" class="statement" type="text" oninput="makeSubstitutions(this)"/>
-    <span v-if="idx == 0 && (node.statements.length > 1 || node.children.length > 1)" @click="expanded = !expanded" class="expand-arrow">{{ expanded ? "▼" : "►" }}</span>
+    <button v-if="idx == 0 && (node.statements.length > 1 || node.children.length > 1)" @click="expanded = !expanded" class="expand-arrow">{{ expanded ? "▼" : "►" }}</button>
   </li>
   <li v-if="(node.statements.length > 1 || node.children.length > 1) && !expanded" class="dots">⋮</li>
   <template v-if="expanded" v-for="child in node.children">
