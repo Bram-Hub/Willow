da4740e0ca2b4597c3ec35098939f620365fba86
Connor Roizman <connorjayr@gmail.com>
2020-03-22 21:15:39 -0400

    Begin adding functionality to toolbar

--- util/middleware.js
+++ util/middleware.js
@@ -1,7 +1,13 @@
+const cp = require("child_process");
+
 /**
  * Injects global constants into the local scope of the Pug renderer.
  */
 exports.injectGlobalLocals = function(req, res, next) {
+  // set the current git commit hash
+  res.locals.commit = cp.execSync("git rev-parse HEAD").toString().trim();
+
+  // set the URL of the Vue.js CDN
   res.locals.assets = {
     vue: process.env.NODE_ENV === "development" ?
         "https://cdn.jsdelivr.net/npm/vue/dist/vue.js" :
