40c1dc6733c7cbbd2f812ef3e997d42ef3bfc2b3
Connor Roizman <connorjayr@gmail.com>
2020-03-24 02:35:15 -0400

    Add more branch manipulation functions, including movement

--- public/js/controls.js
+++ public/js/controls.js
@@ -14,10 +14,78 @@ function focusStatement(branches, offset) {
 }
 
 /**
+ * Focuses on the first statement in the entire tree.
+ */
+function focusFirstStatement() {
+  setTimeout(() => $(".statement").first().focus(), 0);
+}
+
+/**
  * Focuses on the last statement in the entire tree.
  */
 function focusLastStatement() {
   setTimeout(() => $(".statement").last().focus(), 0);
+}
+
+/**
+ * Focuses the statement above the focused statement, or does nothing if no
+ * statement is focused.
+ */
+function moveUp() {
+  if ($(document.activeElement).is(".statement")) {
+    const statements = $(".statement");
+    // search through all statements on the page except for the first, since we
+    // cannot move up from the first one
+    for (let i = 1; i < statements.length; ++i) {
+      if (statements.get(i) === document.activeElement) {
+        // if this statement matches the focused statement, then focus the
+        // statement before this one
+        statements.get(i - 1).focus();
+        return;
+      }
+    }
+  }
+}
+
+/**
+ * Focuses the statement below the focused statement, or does nothing if no
+ * statement is focused.
+ */
+function moveDown() {
+  if ($(document.activeElement).is(".statement")) {
+    const statements = $(".statement");
+    // search through all statements of the page except for the last, since we
+    // cannot move down from the last one
+    for (let i = 0; i < statements.length - 1; ++i) {
+      if (statements.get(i) === document.activeElement) {
+        // if this statement matches the focused statement, then focus the
+        // statement after this one
+        statements.get(i + 1).focus();
+        return;
+      }
+    }
+  }
+}
+
+/**
+ * Adds a blank statement before the focused statement, or to the beginning of the
+ * tree if no statement is focused.
+ */
+function addStatementBefore() {
+  const el = $(document.activeElement);
+  if (el.is(".statement")) {
+    const branches = JSON.parse(el.attr("branches"));
+    const offset = parseInt(el.attr("offset"));
+
+    // append and focus a statement before the focused statement (use element
+    // attributes to determine position)
+    vm.root.child(branches).statements.splice(offset, 0, {str: ""});
+    focusStatement(branches, offset);
+  } else {
+    // prepend and focus a statement to the root node of the tree
+    vm.root.statements.unshift({str: ""});
+    focusFirstStatement();
+  }
 }
 
 /**
@@ -124,17 +192,50 @@ function deleteBranch() {
   focusStatement(branches, parent.statements.length - 1);
 }
 
+/**
+ * Focuses on the parent branch of the one currently focused, or does nothing if
+ * no statements are focused.
+ */
+function endBranch() {
+  const el = $(document.activeElement);
+  if (el.is(".statement")) {
+    // get the parent node with respect to the focused statement
+    const branches = JSON.parse(el.attr("branches"));
+    if (branches.length === 0) {
+      // if the focused statement is within the root branch, do nothing since we
+      // cannot "end" the root branch
+      return;
+    }
+    branches.pop();
+    const parent = vm.root.child(branches);
+
+    // focus the last statement of the parent branch
+    focusStatement(branches, parent.statements.length - 1);
+  }
+}
+
 const shortcuts = [
+  {
+    callback: moveUp,
+    key: 38,
+  },
+  {
+    callback: moveDown,
+    key: 40,
+  },
+  {
+    callback: addStatementBefore,
+    ctrl: true,
+    key: "B",
+  },
   {
     callback: addStatementAfter,
     ctrl: true,
-    shift: false,
     key: "A",
   },
   {
     callback: deleteStatement,
     ctrl: true,
-    shift: false,
     key: "D",
   },
   {
@@ -149,13 +250,27 @@ const shortcuts = [
     shift: true,
     key: "D",
   },
+  {
+    callback: endBranch,
+    ctrl: true,
+    key: "E",
+  },
 ];
 
 document.onkeydown = function(event) {
   for (const shortcut of shortcuts) {
-    if (shortcut.ctrl === event.ctrlKey &&
-        shortcut.shift === event.shiftKey &&
-        shortcut.key.charCodeAt(0) === event.which) {
+    // get the key code for this shortcut (a string or integer may be provided)
+    let key = shortcut.key;
+    if (typeof key === "string" || key instanceof String) {
+      key = key.charCodeAt(0);
+    }
+
+    // ctrl and shift are false by default
+    if ((shortcut.ctrl || false) === event.ctrlKey &&
+        (shortcut.shift || false) === event.shiftKey &&
+        key === event.which) {
+      // if the shortcut is activated, then execute its callback and stop default
+      // behavior (defined by browser)
       shortcut.callback(event);
       event.preventDefault();
       event.stopPropagation();
