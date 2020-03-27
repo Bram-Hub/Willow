1f5977090e3c25ffe765b96a0ec7d5c0d790fc1c
Connor Roizman <connorjayr@gmail.com>
2020-03-27 12:50:39 -0400

    Fix collapse arrow not showing if there is one child branch

--- public/js/index.js
+++ public/js/index.js
@@ -43,9 +43,9 @@ const vm = new Vue({
 <ul class="node-list">
   <li v-for="(statement, idx) in node.statements">
     <input v-if="idx == 0 || expanded" v-model="statement.str" class="statement" type="text" oninput="makeSubstitutions(this)" :branches="JSON.stringify(branches)" :offset="idx"/>
-    <button v-if="idx == 0 && (node.statements.length > 1 || node.children.length > 1)" @click="expanded = !expanded" class="expand-arrow">{{ expanded ? "▼" : "►" }}</button>
+    <button v-if="idx == 0 && (node.statements.length > 1 || node.children.length > 0)" @click="expanded = !expanded" class="expand-arrow">{{ expanded ? "▼" : "►" }}</button>
   </li>
-  <li v-if="(node.statements.length > 1 || node.children.length > 1) && !expanded" class="dots">⋮</li>
+  <li v-if="(node.statements.length > 1 || node.children.length > 0) && !expanded" class="dots">⋮</li>
   <template v-if="expanded" v-for="(child, idx) in node.children">
     <hr class="branch-line">
     <tree-node :node="child" :branches="[...branches, idx]"/>
