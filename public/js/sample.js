95ac303ce7ff86d081c06c2d0ec0dcc32f812aba
Connor Roizman <connorjayr@gmail.com>
2020-04-18 19:09:30 -0400

    Fix file opening

--- public/js/sample.js
+++ public/js/sample.js
@@ -1,20 +1,22 @@
-const root = new TreeNode([
-  "(W∨S) → C",
-  "C ↔ (I∧V)",
-  "¬(¬W → ¬V)",
-  "¬W",
-  "¬¬V",
-  "V",
-], [
-  new TreeNode(["¬(W∨S)", "¬W", "¬S"], [
-    new TreeNode(["C", "I∧V", "I", "V", "o"]),
-    new TreeNode(["¬C", "¬(I∧V)"], [
-        new TreeNode(["¬I", "o"]),
-        new TreeNode(["¬V", "x"]),
+const root = {
+  node: new TreeNode([
+    "(W∨S) → C",
+    "C ↔ (I∧V)",
+    "¬(¬W → ¬V)",
+    "¬W",
+    "¬¬V",
+    "V",
+  ], [
+    new TreeNode(["¬(W∨S)", "¬W", "¬S"], [
+      new TreeNode(["C", "I∧V", "I", "V", "o"]),
+      new TreeNode(["¬C", "¬(I∧V)"], [
+          new TreeNode(["¬I", "o"]),
+          new TreeNode(["¬V", "x"]),
+      ]),
+    ]),
+    new TreeNode(["C"], [
+      new TreeNode(["C", "I∧V", "I", "V", "o"]),
+      new TreeNode(["¬C", "¬(I∧V)", "x"]),
     ]),
   ]),
-  new TreeNode(["C"], [
-    new TreeNode(["C", "I∧V", "I", "V", "o"]),
-    new TreeNode(["¬C", "¬(I∧V)", "x"]),
-  ]),
-]);
+};
