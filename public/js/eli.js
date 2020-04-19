5bc65bf7d239a5a42921c1b465d2ebaebb3c5ae6
Connor Roizman <connorjayr@gmail.com>
2020-04-19 04:48:37 -0400

    Move valid marks to left side of vertical line

--- public/js/eli.js
+++ public/js/eli.js
@@ -1,8 +1,8 @@
-const root = new TreeNode([
-  "(W∨S) → C",
-], [
-  new TreeNode(["(W∨S)"], [
-    new TreeNode(["W", "S"], []),
+const root = {
+  node: new TreeNode(["(W∨S) → C"], [
+    new TreeNode(["(W∨S)"], [
+      new TreeNode(["W", "S"]),
+    ]),
+    new TreeNode(["C"]),
   ]),
-  new TreeNode(["C"], []),
-]);
+};
