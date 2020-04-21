174cbd9f528700e4ad94a515edd298144dbae07e
Connor Roizman <connorjayr@gmail.com>
2020-04-21 09:00:05 -0400

    Fix Eli's tree

--- public/js/eli.js
+++ public/js/eli.js
@@ -1,7 +1,7 @@
 const root = {
   node: new TreeNode(["(W∨S) → C"], [
     new TreeNode(["¬(W∨S)"], [
-      new TreeNode(["W", "S"]),
+      new TreeNode(["¬W", "¬S"]),
     ]),
     new TreeNode(["C"]),
   ]),
