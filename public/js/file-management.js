8cb7e95954b8b0acc80d7b422bb3aef1ee792704
Ben Kelly <benjaminkelly@me.com>
2020-04-27 19:31:23 -0400

    added importing from TFTrees

--- public/js/file-management.js
+++ public/js/file-management.js
@@ -22,6 +22,18 @@ function openFile() {
 }
 
 /**
+ * Opens a file browser, which will eventually load the tree.
+ */
+function openImportFile() {
+  if (!confirm(
+      "Opening a file will overwrite the current tree. Are you sure?"
+  )) {
+    return;
+  }
+  $("#open-import-file").click();
+}
+
+/**
  * Loads the selected file to the tree.
  */
 function loadFile(event) {
@@ -208,4 +220,199 @@ function exportToTFT() {
 
   const blob = new Blob([xml_string], {type: "text/plain;charset=utf-8"});
   saveAs(blob, vm.name + ".tft");
+}
+
+function handle_branch(node, map, index, branch_arr, ignore_statements) {
+  let branch_index = 0
+  let offset = 0
+  for (const child of node.children) {
+    if ((child.tagName === "BranchLine" || child.tagName === "Terminator") && !ignore_statements) {
+      map[index] = "{\"branches\":[" + branch_arr.toString() + "],\"offset\":" + offset + "}"
+      index += 1
+      offset += 1
+    }
+    else if (child.tagName === "Branch") {
+      branch_arr_copy = [...branch_arr]
+      branch_arr_copy.push(branch_index)
+      index = handle_branch(child, map, index, branch_arr_copy, false)
+      branch_index += 1
+    }
+  }
+  return index
+}
+
+function make_line_to_ref_mapping(xmlDoc, map){
+  let dom_root = xmlDoc.querySelector("Tree");
+  let index = 0
+  for (const child of dom_root.children) {
+    if (child.tagName === "BranchLine" || child.tagName === "Terminator") {
+      map[index] = "{\"branches\":[],\"offset\":" + index + "}"
+      index += 1
+    }
+    if (child.tagName === "Branch") {
+      let branch_index = 0;
+      for (const grandchild of child.children) {
+        if (grandchild.tagName === "BranchLine" || grandchild.tagName === "Terminator") {
+          map[index] = "{\"branches\":[],\"offset\":" + index + "}"
+          index += 1
+        }
+        if (grandchild.tagName === "Branch") {
+          index = handle_branch(grandchild, map, index, [branch_index], false)
+          branch_index += 1
+        }
+      }
+    }
+  }
+} 
+
+function parseBranch(node, line_map) {
+  let branch_node = new TreeNode();
+  let index = 0
+  for (child of node.children) {
+    if (child.tagName === "BranchLine") {
+      let statement = {}
+      statement.str = child.getAttribute("content");
+      statement.premise = false
+      statement.references = []
+      for (const grandchild of child.children) {
+        if (grandchild.tagName === "Decomposition") {
+          if (grandchild.hasAttribute("branchIndex")) {
+            continue;
+          }
+          let line_index = parseInt(grandchild.getAttribute("lineIndex"))
+          let ref = line_map[line_index]
+          statement.references.push(ref)
+        }
+      }
+      branch_node.statements.push(statement)
+    }
+    else if (child.tagName === "Terminator") {
+      let statement = {}
+      statement.premise = false
+      statement.references = []
+      if (child.getAttribute("close") === "true") {
+        statement.str = "×"
+      }
+      else {
+        statement.str = "◯"
+      }
+      for (const grandchild of child.children) {
+        if (grandchild.tagName === "Decomposition") {
+          if (grandchild.hasAttribute("branchIndex")) {
+            continue;
+          }
+          let line_index = parseInt(grandchild.getAttribute("lineIndex"))
+          let ref = line_map[line_index]
+          statement.references.push(ref)
+        }
+      }
+      branch_node.statements.push(statement)
+    }
+    else if (child.tagName === "Branch") {
+      branch_node.children.push(parseBranch(child, line_map))
+      index += 1
+    }
+  }
+  return branch_node;
+}
+
+function parseTFT(content) {
+  const parser = new DOMParser();
+  const xmlDoc = parser.parseFromString(content,"text/xml");
+  let dom_root = xmlDoc.querySelector("Tree");
+  
+  let line_map = {}
+  make_line_to_ref_mapping(xmlDoc, line_map);
+
+  let tree_root = new TreeNode();
+  let index = 0;
+  for (const child of dom_root.children) {
+    if (child.tagName === "BranchLine") {
+      let statement = {}
+      statement.str = child.getAttribute("content");
+      statement.premise = true
+      statement.references = []
+      for (const grandchild of child.children) {
+        if (grandchild.tagName === "Decomposition") {
+          if (grandchild.hasAttribute("branchIndex")) {
+            continue;
+          }
+          let line_index = parseInt(grandchild.getAttribute("lineIndex"))
+          let ref = line_map[line_index]
+          statement.references.push(ref)
+        }
+      }
+      tree_root.statements.push(statement)
+    }
+    else if (child.tagName === "Branch") {
+      for (const deep_child of child.children) {
+        if (deep_child.tagName === "BranchLine") {
+          let statement = {}
+          statement.str = deep_child.getAttribute("content");
+          statement.premise = false
+          statement.references = []
+          for (const grandchild of deep_child.children) {
+            if (grandchild.tagName === "Decomposition") {
+              if (grandchild.hasAttribute("branchIndex")) {
+                continue;
+              }
+              let line_index = parseInt(grandchild.getAttribute("lineIndex"))
+              let ref = line_map[line_index]
+              statement.references.push(ref)
+            }
+          }
+          tree_root.statements.push(statement)
+        }
+        else if (deep_child.tagName === "Terminator") {
+          let statement = {}
+          statement.premise = false
+          statement.references = []
+          if (deep_child.getAttribute("close") === "true") {
+            statement.str = "×"
+          }
+          else {
+            statement.str = "◯"
+          }
+          for (const grandchild of child.children) {
+            if (grandchild.tagName === "Decomposition") {
+              if (grandchild.hasAttribute("branchIndex")) {
+                continue;
+              }
+              let line_index = parseInt(grandchild.getAttribute("lineIndex"))
+              let ref = line_map[line_index]
+              statement.references.push(ref)
+            }
+          }
+          tree_root.statements.push(statement)
+        }
+        else if (deep_child.tagName === "Branch") {
+          tree_root.children.push(parseBranch(deep_child, line_map))
+          index += 1
+        }
+      }
+    }
+  }
+  vm.root.node = tree_root;
+}
+
+function loadImportFile(event) {
+  // retrieve the file reference
+  const file = event.target.files[0]; 
+
+  // setting up the reader
+  const reader = new FileReader();
+  reader.readAsText(file, "UTF-8");
+
+  // load content when reader is done reading the file
+  reader.onload = function(readerEvent) {
+    let filename = file.name;
+    if (filename.endsWith(".tft")) {
+      filename = filename.substring(0, filename.length - 5);
+    }
+    vm.name = filename;
+
+    const content = readerEvent.target.result;
+    parseTFT(content);
+
+  }
 }
