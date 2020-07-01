15504691946c2516144f42aacfb8f0387c1d48ce
Connor Roizman <connorjayr@gmail.com>
2020-06-30 21:46:58 -0400

    Refactor server-side code

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// import environment variables from .env into process.env
+// Import environment variables from .env into process.env
 require("dotenv").config();
 
 const index = require("./routes/index");
@@ -7,70 +7,75 @@ const middleware = require("./util/middleware");
 const express = require("express");
 
 /**
- * Configures properties for an Express application.
+ * Configures an Express application.
  * 
- * @param {express.app} app the Express application to configure
+ * @param {express.app} app the application
  */
 function configureApp(app) {
-  // use Pug.js as the template engine
+  // Use Pug.js as the template engine
   app.set("view engine", "pug");
-  // retrieve templates from the views/ directory
+  // Retrieve templates from the views/ directory
   app.set("views", "views/");
-  // set base directory for Pug.js to the current working directory
+  // Set base directory for Pug.js to the current working directory
   app.locals.basedir = __dirname;
-  // expose the /public directory and its contents to clients, if necessary
+  // Expose the /public directory and its contents to clients, if necessary
   app.use(express.static(__dirname + "/public"));
 
-  app.use(middleware.injectGlobalLocals);
+  app.use(middleware.injectLocals);
 }
 
 /**
- * Launches the Express application on the provided port(s), and launches an HTTP
- * redirect server (if applicable).
+ * Launches the Express application on the provided port(s), and launches an
+ * HTTP redirect server if an HTTPS web server is launched.
  * 
- * @param {express.app} app the Express application to launch
- * @param {object} ports an object containing the port(s) on which to launch the
- * application
+ * @param {express.app} app the application
+ * @param {object} ports an object containing the port(s) on which to launch
+ *     the application
+ * @param {number} [ports.http] the port on which to launch the HTTP web
+ *     server, or the HTTPS redirect server if `ports.https` is defined
+ * @param {number} [ports.https] the port on which to launch the HTTPS web
+ *     server
  */
 function launchServer(app, ports) {
   if (ports.http && ports.https) {
-    // if both HTTP and HTTPS ports are provided, launch an HTTP redirect server
+    // If both HTTP and HTTPS ports are provided, launch an HTTP redirect server
     require("http").createServer((req, res) => {
       res.writeHead(301, {Location: "https://" + req.headers.host + req.url});
       res.end();
     }).listen(ports.http);
   } else if (ports.http) {
     app.listen(
-        ports.http, () => console.log("Server launched on port " + ports.http)
+        ports.http, () => console.log("[INFO] Server launched on port " + ports.http)
     );
   }
   if (ports.https) {
     require("https").createServer({
-      // TODO: retrieve HTTPS certificate
+      // TODO: Retrieve HTTPS certificate
     }, app).listen(
-        ports.https, () => console.log("Server launched on port " + ports.https)
+        ports.https, () => console.log("[INFO] Server launched on port " + ports.https)
     );
   }
 }
 
-// initialize Express application
+// Initialize Express application
 const app = express();
 configureApp(app);
 
-// configure routing for application
+// Configure web server routes
 app.get("/", index.get);
 app.get("/assignments", (req, res) => res.render("assignments"));
 app.get("/auth/login", (req, res) => res.render("auth/login"));
-// lowest priority request matches everything and returns a 404 error
+// Fallback to 404 error
 app.get("*", (req, res) => res.status(404).render("404"));
 
 if (!process.env.HTTP_PORT && !process.env.HTTPS_PORT) {
+  // If no ports were configured for the web server, then exit the application
   console.error(
-      "[ERROR] no ports configured for web server, see .env-template"
+      "[ERROR] No ports configured for web server, see .env-template"
   );
   process.exit(1);
 }
 
-// get server ports from .env
+// Read the web server ports from .env
 const ports = {http: process.env.HTTP_PORT, https: process.env.HTTPS_PORT};
 launchServer(app, ports);
--- util/middleware.js
+++ util/middleware.js
@@ -1,14 +1,15 @@
 const cp = require("child_process");
 
 /**
- * Injects global constants into the local scope of the Pug renderer.
+ * Injects global constants into the local scope of the Pug templating engine.
  */
-exports.injectGlobalLocals = function(req, res, next) {
-  // set the current git commit hash
+exports.injectLocals = function(req, res, next) {
+  // Inject the hash string for the latest commit
   res.locals.commit = cp.execSync("git rev-parse HEAD").toString().trim();
 
-  // set the URL of the Vue.js CDN
+  // Inject the URLs for libraries
   res.locals.assets = {
+    // The URL for Vue.js changes based on the environment
     vue: process.env.NODE_ENV === "development" ?
         "https://cdn.jsdelivr.net/npm/vue/dist/vue.js" :
         "https://cdn.jsdelivr.net/npm/vue@2.6.11",
