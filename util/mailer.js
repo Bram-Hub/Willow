d9aa058a9294876a7990e1e6ff9d3f2d1f892c16
Connor Roizman <connorjayr@gmail.com>
2020-07-19 22:53:43 -0400

    Implement nodemailer (not used to send emails yet)

---
+++ util/mailer.js
@@ -0,0 +1,11 @@
+const nodemailer = require('nodemailer');
+
+exports.transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: process.env.SMTP_SECURE === 'true',
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
