593d7fa4f13fe3de34f8304823b9ecdc2e3197b9
Eli Schiff <eli@elischiff.org>
2021-02-05 11:55:25 -0500

    Add changes to make Willow work with heroku (#23)
    * Fix port for heroku
    
    * Fix broken port issues
    
    * fix issue with git not existing
    
    * Clean up heroku code
    
    * More fixes
    
    * fix git issue
    
    * fix
    
    * Fix git on heroku

--- util/middleware.js
+++ util/middleware.js
@@ -21,7 +21,13 @@ exports.injectLocals = async function(req, res, next) {
   };
 
   // Inject the hash string for the latest commit
-  res.locals.commit = cp.execSync('git rev-parse HEAD').toString().trim();
+  try {
+    res.locals.commit = cp.execSync('git rev-parse HEAD').toString().trim();
+  } catch (e) {
+    // The above command will fail in Heroku
+    // In theory Heroku provides a `SOURCE_VERSION` but I cannot get it working
+    res.locals.commit = "Production";
+  }
 
   // Inject the session information
   const email = objects.nestedProperty(req, 'session.email');
