8fe9574fad20a91c193d82bd06f4f220ba31d7b5
Eli Schiff <eli@elischiff.org>
2021-04-25 16:18:37 -0400

    Add a warning for nonparsable statements to match fitch

--- src/client/component/truth-tree-node.ts
+++ src/client/component/truth-tree-node.ts
@@ -9,10 +9,19 @@ export const TruthTreeNodeComponent: vue.Component = {
 		node() {
 			return this.$store.state.tree.nodes[this.id];
 		},
+		parsable():string {
+			for(const value of <[string]>Object.values(this.node.isValid())){
+				if(value.endsWith("is not a parsable statement.")){
+					return value.split(': ')[1];
+				}
+			}
+			return "";
+		}
 	},
 	template: `
     <span v-if="$store.state.developerMode">id: {{ id }} valid: {{ JSON.stringify(node.isValid()) }} decomposed: {{ JSON.stringify(node.isDecomposed()) }} universe: {{ JSON.stringify(Array.from(node.universe).map(formula => formula.toString()) ) }} </span>
-    <i v-if="Object.keys(node.isValid()).length === 0 && Object.keys(node.isDecomposed()).length === 0" class="fas fa-check"></i>
+    <i v-if="parsable !== ''" class="fas fa-exclamation-triangle" :title="parsable"></i>
+    <i v-else-if="Object.keys(node.isValid()).length === 0 && Object.keys(node.isDecomposed()).length === 0" class="fas fa-check"></i>
     <i v-else class="fas fa-times"></i>
     <input :id='"node" + this.id' type="text" v-model="node.text" :class="{
       'statement': true,
