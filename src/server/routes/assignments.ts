2708e08b69601e154b5db36795cce15b65c0b3df
Connor Roizman <connorjayr@gmail.com>
2021-10-12 12:45:53 -0400

    Reformat SQL queries

--- src/server/routes/assignments.ts
+++ src/server/routes/assignments.ts
@@ -13,25 +13,25 @@ export async function get(req: express.Request, res: express.Response) {
 		return res.redirect('/');
 	}
 
+	// Retrieve all assignments for every course of which this user is a student
 	const assignments: Pick<
 		AssignmentsRow,
-		'course_name' | 'due_date' | 'name'
+		'name' | 'course_name' | 'due_date'
 	>[] = (
 		await db.query(
 			`
                 SELECT
                     "assignments"."name",
-                    COALESCE("courses"."display_name", "assignments"."course_name") AS "course_name",
+                    COALESCE("courses"."display_name", "courses"."name")
+						AS "course_name",
                     "assignments"."due_date"
                 FROM "assignments"
                 INNER JOIN "courses"
-                    ON (assignments.course_name = courses.name)
+                    ON "courses"."name" = "assignments"."course_name"
                 INNER JOIN "students"
-                    ON (courses.name = students.course_name)
+					ON "courses"."name" = "students"."course_name"
                 WHERE "students"."student_email" = $1
-                ORDER BY
-                    "assignments"."due_date" ASC NULLS LAST,
-                    "assignments"."created_at" ASC
+                ORDER BY "assignments"."due_date", "assignments"."created_at"
 		    `,
 			[req.user.email]
 		)
--- src/server/routes/courses.ts
+++ src/server/routes/courses.ts
@@ -13,13 +13,14 @@ export async function get(req: express.Request, res: express.Response) {
 		return res.redirect('/');
 	}
 
+	// Retrieve all courses of which this user is a student
 	const courses: Pick<CoursesRow, 'display_name'>[] = (
 		await db.query(
 			`
 				SELECT COALESCE("courses"."display_name", "courses"."name") AS "display_name"
 				FROM "courses"
 				INNER JOIN "students"
-					ON (courses.name = students.course_name)
+					ON "courses"."name" = "students"."course_name"
 				WHERE "students"."student_email" = $1
 				ORDER BY "courses"."created_at" DESC
 			`,
