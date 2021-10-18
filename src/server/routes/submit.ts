3c9c0c565520d0bb9e8723270c30b5060c6125be
Connor Roizman <connorjayr@gmail.com>
2021-10-18 17:49:28 -0400

    Import TruthTree in submit.ts

--- src/server/routes/submit.ts
+++ src/server/routes/submit.ts
@@ -1,6 +1,6 @@
 import * as express from 'express';
 
-// import {TruthTree} from 'common/tree';
+import {TruthTree} from 'common/tree';
 import {pool as db} from 'server/util/database';
 import * as schemas from 'server/util/schemas';
 import {PostRequest} from 'types/routes/submit/post-request';
@@ -22,15 +22,15 @@ router.post('/', async (req, res) => {
 	const body: PostRequest = req.body as any;
 
 	// Validate the tree
-	const correct = false;
-	// try {
-	// 	const tree = TruthTree.deserialize(body.tree);
-	// 	correct = tree.isCorrect().value;
+	let correct = false;
+	try {
+		const tree = TruthTree.deserialize(body.tree);
+		correct = tree.isCorrect().value;
 
-	// 	// TODO: validate that the tree matches the options / premises of the assignment
-	// } catch (err) {
-	// 	return res.redirect('/?error=invalid_tree');
-	// }
+		// TODO: validate that the tree matches the options / premises of the assignment
+	} catch (err) {
+		return res.redirect('/?error=invalid_tree');
+	}
 
 	// Add the submission to the database
 	try {
