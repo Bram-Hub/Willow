3814abfcaf571db2548a9fe36c9f4570b043812b
Connor Roizman <connor@roizman.org>
2024-03-16 16:10:54 -0400

    Update dependencies
    Update all dependencies to their latest versions.

--- src/server/routes/auth/logout.ts
+++ src/server/routes/auth/logout.ts
@@ -2,7 +2,11 @@ import * as express from 'express';
 
 export const router = express.Router();
 
-router.get('/', (req, res) => {
-	req.logOut();
-	res.redirect('/');
+router.get('/', (req, res, next) => {
+	req.logOut((err) => {
+		if (err) {
+			return next(err);
+		}
+		res.redirect('/');
+	});
 });
